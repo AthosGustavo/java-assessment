9a02e0fd823b7eac09671906ed10cca1b40ff38a
Athos Gustavo <athosgustavowf@gmail.com>
2025-02-23 21:36:34 -0300

    Ajustes finais

--- backend/src/main/java/br/novo/Main.java
+++ backend/src/main/java/br/novo/Main.java
@@ -9,6 +9,7 @@ import org.hibernate.cfg.Configuration;
 import spark.Spark;
 
 import static spark.Spark.before;
+import static spark.Spark.options;
 
 public class Main {
   public static void main(String[] args) {
@@ -18,7 +19,13 @@ public class Main {
       response.header("Access-Control-Allow-Origin", "*");
       response.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
       response.header("Access-Control-Allow-Headers", "*");
+      response.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept");
       response.header("Access-Control-Allow-Credentials", "true");
+    });
+
+    options("/*", (request, response) -> {
+      response.status(200);
+      return "";
     });
 
     LivroController livroController = new LivroController();
